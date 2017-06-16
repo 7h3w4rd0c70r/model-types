19b2c25d86fc187832c929f51ac1dc5ab93affb0
Patrik Šimunič <patriksimunic@hotmail.com>
2017-06-16 16:27:53 +0200

    v1.5.0

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import babel from 'rollup-plugin-babel'
 
 export default {
     entry: 'src/index.js',
-    dest: 'dist/model-types.js',
+    dest: 'dist/index.js',
     format: 'cjs',
     plugins: [
         babel({
